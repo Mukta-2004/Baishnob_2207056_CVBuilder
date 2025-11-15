56acd4517db6321d7f41642d9082d9caaf94fdb4
Mukta-2004 <muktabaishnob16@gmail.com>
2025-11-15 18:15:22 +0600

    add coverPage

--- CV_Builder/src/main/java/com/example/cv_builder/coverpageController.java
+++ CV_Builder/src/main/java/com/example/cv_builder/coverpageController.java
@@ -1,4 +1,26 @@
 package com.example.cv_builder;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class coverpageController {
+
+    @FXML
+    private void nextPage(ActionEvent event) throws IOException {
+        System.out.println("Next button clicked!");
+
+        Parent nextRoot = FXMLLoader.load(getClass().getResource("nextpage.fxml"));
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(new Scene(nextRoot));
+        stage.show();
+    }
 }
