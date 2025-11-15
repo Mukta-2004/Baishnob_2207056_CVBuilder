14981394a6d88dc5d6120f7843dbd77008134cb1
Mukta-2004 <muktabaishnob16@gmail.com>
2025-11-15 10:17:00 +0600

    add coverpage

--- CV_Builder/src/main/java/com/example/cv_builder/HelloController.java
+++ CV_Builder/src/main/java/com/example/cv_builder/HelloController.java
@@ -1,14 +1,15 @@
-package com.example.cv_builder;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
+//package com.example.cv_builder;
+//
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Label;
+//
+//public class HelloController {
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+//}
